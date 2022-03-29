aec85b23b07d7658c4c5c970c3199c7c92fa0e25
sakastudio <sakastudio100@gmail.com>
2022-03-29 14:56:52 +0900

    タイトルをちょっと修正

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'moorestech documentation',
+  tagline: 'Documentation of moorestech development',
   url: 'https://moorestech.github.io',
   baseUrl: '/moorestech_doc/',
   onBrokenLinks: 'throw',
