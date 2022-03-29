27eefd4f13d3c4d2c8173ba526476923e29ce982
sakastudio <sakastudio100@gmail.com>
2022-03-29 14:11:55 +0900

    change config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,13 +8,13 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://moorestech.github.io',
+  baseUrl: '/moorestech_doc/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'moorestech', // Usually your GitHub org/user name.
+  projectName: 'moorestech_doc', // Usually your repo name.
 
   presets: [
     [
