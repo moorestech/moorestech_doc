68167f2ad76543a970260af798bebd0d80205251
sakastudio <sakastudio100@gmail.com>
2022-03-30 14:08:12 +0900

    ブログはコメントアウトしとく

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -55,6 +55,7 @@ const config = {
             position: 'left',
             label: 'Tutorial',
           },
+          {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/moorestech',
             label: 'GitHub',
