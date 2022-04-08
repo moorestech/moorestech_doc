928b7b384ef3863b502b420f12c0284bca83b671
sakastudio <sakastudio100@gmail.com>
2022-04-08 09:39:11 +0900

    edit pageの修正

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,13 +24,13 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/moorestech/moorestech_doc/tree/master/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/moorestech/moorestech_doc',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
