4898cf184a2346e21847b24bb9939537d508a65e
sakastudio <katsumi.sato@aktsk.jp>
2024-12-29 21:40:55 +0900

    新docusurusに移行

--- i18n/en/docusaurus-plugin-content-docs/current/intro.md
+++
@@ -1,7 +0,0 @@
----
-sidebar_position: 1
----
-
-# Intro
-This document is a document about technical information about moorestech.  
-It will be updated as needed.
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,26 @@
-import React from 'react';
 import clsx from 'clsx';
-import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css';
+import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import Heading from '@theme/Heading';
+
+import styles from './index.module.css';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
+        <Heading as="h1" className="hero__title">
+          {siteConfig.title}
+        </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="https://wiki.moores.tech/wiki/Official_moorestech_wiki">
-            新しいWikiを見る
+            to="/docs/intro">
+            Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div>
       </div>
@@ -29,7 +32,8 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`moorestech doc`}>
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
